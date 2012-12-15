4a7413791753dc13ab716f4b074cb81b15db88c6
alvin-chae <dbrnl04@lycos.co.kr>
2012-12-15 23:23:43 +0900

    test01

--- eee/src/com/example/eee/MainActivity.java
+++ eee/src/com/example/eee/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	String Test;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
